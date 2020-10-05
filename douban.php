dae1b199de131622d6a1040a7135dfe1cdad7840
小航 <38235544+xhboke@users.noreply.github.com>
2020-10-05 19:53:53 +0800

    Update douban.php
    补充豆瓣取播放链接情形

--- douban.php
+++ douban.php
@@ -16,7 +16,6 @@ if ($Type == 'movie') {
     $douban->Movie($mode, $sort, '24', $page);
     print_r(json_encode($douban->GetMovieArr));
 } elseif ($Type == 'tv') {
-    $douban = new douban;
     $mode = isset($_GET['mode']) ? $_GET['mode'] : ''; //'热门','美剧','英剧'
     $sort = isset($_GET['sort']) ? $_GET['sort'] : ''; //热度：recommend时间：time 评价：rank
     $page = isset($_GET['page']) ? $_GET['page'] : exit; //热度：recommend时间：time 评价：rank
