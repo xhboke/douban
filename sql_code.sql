5b88eeea365aa8b605b4a7618e2bcc8e511c91c9
xhboke <xh@apa.me>
2020-12-17 23:48:15 +0800

    :fire:增添 SQL 建表文件

---
+++ sql_code.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Movies (
+    id int primary key,
+    title varchar(1000),
+    describe varchar(MAX),
+    datepublished varchar(255),
+    genre varchar(255),
+    rating int,
+    votes int,
+    img varchar(255),
+    years varchar(255),
+    actor varchar(MAX),
+    director varchar(MAX),
+)
