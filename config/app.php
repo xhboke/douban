ece9215ecbac245898761f953dd0655e39a27213
小小航 <xhapame@gmail.com>
2021-10-06 20:50:22 +0800

    cache

--- config/app.php
+++ config/app.php
@@ -117,9 +117,9 @@ return [
     // 表单pjax伪装变量
     'var_pjax' => '_pjax',
     // 是否开启请求缓存 true自动缓存 支持设置请求缓存规则
-    'request_cache' => false,
+    'request_cache' => true,
     // 请求缓存有效期
-    'request_cache_expire' => null,
+    'request_cache_expire' => 604800,
     // 全局请求缓存排除规则
     'request_cache_except' => [],
     // 是否开启路由缓存
